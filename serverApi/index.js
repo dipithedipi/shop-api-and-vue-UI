7d6439ff1711a95baca9ca055ac82bc8f5baac59
Diploma Francesco <Diploma.19127>
2024-02-15 09:18:22 +0100

    Added login for customers with jwt

--- serverApi/index.js
+++ serverApi/index.js
@@ -1,10 +1,15 @@
-//products , customers, emplojers
 import express from 'express';
 import cors from 'cors';
 import mysql from 'mysql';
 import swaggerUI from 'swagger-ui-express';
 import yaml from 'yamljs';
 import 'dotenv/config';
+import sanitizer from "perfect-express-sanitizer";
+import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken';
+import sha256 from 'crypto-js';
+
+
 
 const swaggerDocument = yaml.load('./swagger.yaml');
 
@@ -22,6 +27,14 @@ const db = mysql.createConnection({
 });
 
 app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.use(
+    sanitizer.clean({
+      xss: true,
+      noSql: true,
+      sql: true,
+    })
+  );
 
 db.connect((err) => {
     if (err) {
@@ -85,6 +98,39 @@ app.get('/employees/:email/:password', (req, res) => {
             return;
         }
         res.status(200).json(result);
+    });
+});
+
+//login for customers
+app.post("/login/customer", (req, res) => {
+
+    let email = req.body.email;
+    let password = req.body.password;
+    let hashedPassword = sha256(String(password + PASSWORD_SALT)).toString();    
+
+    db.query('SELECT customers.* FROM customers WHERE email = ? AND password = ?', [email, hashedPassword], (err, result) => {
+        if (err) {
+            console.error('[!] Error: ' + err.stack);
+            res.status(500).json({ message: "Database query error!" });
+            return;
+        }
+
+        jwt.sign(
+            { 
+                id: result.customerNumber, 
+                name: result.customerName 
+            }, 
+            process.env.JWT_SECRET, 
+            {
+                expiresIn: process.env.JWT_EXPIRES_IN
+            }, (err, token) => {
+            if (err) {
+                console.error('[!] Error: ' + err.stack);
+                res.status(500).json({ message: "Error creating token!" });
+                return;
+            }
+            res.status(200).json({ token: token, user: result });
+        });
     });
 });
 
